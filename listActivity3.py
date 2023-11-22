902042fe3f926727b9d65b3270e0fe8c70655c74
NajmaSulaiman <najmasulaiman1999@gmail.com>
2023-11-22 11:42:49 +0400

    list function activity

--- listActivity3.py
+++ listActivity3.py
@@ -1,45 +1,27 @@
-#def main():
+counter = []
+x = [1, 2, 3, 4, 5, 6]
+
+def main():
     
-    
-    
-    
-counter=[]
-x=[1,2,3,4,5,6]
-def countInputs(side):
-    
-    
-    while(True):
-        n=input("enter a number: ")
-        if(n!="Q"):
+    countInputs(counter)
+    printCounters(counter, x)
+
+def countInputs(counter):
+    while True:
+        n = input("enter a number (1-6) or Q=for stop: ")
+        if n != "Q":
             counter.append(int(n))
         else:
-            #print(counter)
             break
     
-    return counter
-a=countInputs(counter)
-print(a)
-"""
-one=a.count(1)
-two=a.count(2)
-three=a.count(3)
-four=a.count(4)
-five=a.count(5)
-six=a.count(6)
-print("number of 1: ",one)"""
-for i in a:
-    print(x,":",a.count(i))
 
-   
-#def printCounters(counter):
-    
+def printCounters(counter, x):
+    for i in x:
+        #print("{}: {}".format(i, counter.count(i)))
+        print(i," : " ,counter.count(i))
         
         
-    
-    
-    
-    
-    
-    
-#main()   
-    
+
+
+main()
+
